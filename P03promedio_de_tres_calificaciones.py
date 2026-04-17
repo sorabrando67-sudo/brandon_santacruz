d6be39be93e501d9a3fac65a4e49b5503507d3e0
sorabrando67-sudo <sorabrando67@gmail.com>
2026-04-17 17:13:09 -0600

    Add files via upload

---
+++ P03promedio_de_tres_calificaciones.py
@@ -0,0 +1,16 @@
+# Pedir las tres calificaciones
+cal1 = float(input("Ingresa la primera calificación: "))
+cal2 = float(input("Ingresa la segunda calificación: "))
+cal3 = float(input("Ingresa la tercera calificación: "))
+
+# Calcular el promedio
+promedio = (cal1 + cal2 + cal3) / 3
+
+# Mostrar el promedio
+print("Tu promedio es:", promedio)
+
+# Evaluar si aprueba o reprueba
+if promedio >= 6:
+    print("Aprobado")
+else:
+    print("Reprobado")
