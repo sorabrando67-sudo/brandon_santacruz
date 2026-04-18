b505838c0db0a8f187a98a6a2806fc092887e0d0
sorabrando67-sudo <sorabrando67@gmail.com>
2026-04-18 17:14:45 -0600

    Add files via upload

---
+++ P07Funciones_ya_establecidas.py
@@ -0,0 +1,6 @@
+numeros = [5, 10, 15, 20]
+
+print("Suma:", sum(numeros))      # suma los valores
+print("Máximo:", max(numeros))    # número mayor
+print("Mínimo:", min(numeros))    # número menor
+print("Cantidad:", len(numeros))  # cantidad de elementos
